e28a19e4e9e0fda357d5de7352f19061b4d45eee
James Leung <jleung128hk@gmail.com>
2019-11-17 18:36:49 -0500

    API for photo upload

---
+++ Reactivities/Persistence/Migrations/20191117232807_PhotoEntityCorrected.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class PhotoEntityCorrected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "MyProperty");
+        }
+    }
+}
