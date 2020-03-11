82c7d4c34a545a70c7b1fbe398a8302902388459
wortelitewort <jleung128hk@gmail.com>
2020-03-11 17:53:50 -0400

    Added two tiny console apps for convenience

---
+++ JupyterLab/JupyterLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JupyterLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C conda run jupyter lab");
+        }
+    }
+}
--- NorthWindProject/NorthwindMvc/Data/ApplicationDbContext.cs
+++ NorthWindProject/NorthwindMvc/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindMvc.Data
-{
+{ 
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
