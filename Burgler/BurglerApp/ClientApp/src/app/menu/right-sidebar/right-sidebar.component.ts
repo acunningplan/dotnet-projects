c86d9a6dffb8a50d9084d3073a6fc84a6a543026
wortelitewort <jleung128hk@gmail.com>
2020-05-07 15:06:54 -0400

    Show current order; can delete items from order

--- Burgler/BurglerApp/ClientApp/src/app/menu/right-sidebar/right-sidebar.component.ts
+++ Burgler/BurglerApp/ClientApp/src/app/menu/right-sidebar/right-sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { OrderService } from "src/app/orders/order.service";
+import { Order } from "src/app/orders/order";
 
 @Component({
   selector: "app-right-sidebar",
@@ -6,10 +8,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./right-sidebar.component.css"],
 })
 export class RightSidebarComponent implements OnInit {
-
-  constructor() {}
+  @Input() order: Order;
+  constructor(private orderService: OrderService) {
+    // this.order = orderService.getPendingOrder();
+  }
 
   ngOnInit() {}
 
-  onClick() {}
+  onDeleteItem(type: string, name: string, size: string) {
+    const order = this.order;
+    if (type === "burger") {
+      console.log(order);
+      order.burgerItems = order.burgerItems.filter(
+        (b) => b.name !== name || b.size !== size
+      );
+      console.log(order);
+    } else if (type === "side") {
+      order.sideItems = order.sideItems.filter(
+        (s) => s.name !== name || s.size !== size
+      );
+    } else if (type === "drink") {
+      order.drinkItems = order.drinkItems.filter(
+        (d) => d.name !== name || d.size !== size
+      );
+    }
+    this.orderService.updatePendingOrder(order);
+  }
 }
