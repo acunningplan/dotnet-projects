24add13caa9b0e0e413f7f8d61bd5fbcaf30c6e7
James Leung <jlfly12@gmail.com>
2020-09-04 09:34:42 +0100

    "Added FB login types"

--- TravelBug/TravelBug/ClientApp/src/app/account/social-login/social-login.component.ts
+++ TravelBug/TravelBug/ClientApp/src/app/account/social-login/social-login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { AccountService } from "src/app/services/account.service";
 import { GoogleProfile } from "../../models/profiles";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 // import "@types/gapi2";
 // import "@types/gapi2.auth2";
 // declare const gapi: any;
@@ -13,10 +15,15 @@ import { GoogleProfile } from "../../models/profiles";
   styleUrls: ["./social-login.component.css"],
 })
 export class SocialLoginComponent implements OnInit {
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit() {
     this.googleInit();
+    FB.init({ appId: environment.fbAppId });
   }
 
   private googleInit() {
@@ -42,7 +49,25 @@ export class SocialLoginComponent implements OnInit {
     });
   }
 
-  private fbLogin() {
+  fbLogin() {
+    FB.login((response) => {
+      console.log(response);
+      // this.router.navigate(["/"]);
+
+      FB.api("/me", (res) => console.log(res));
+
+      // const url = `https://graph.facebook.com/${response.authResponse.userID}?fields=id,name,birthday,email&access_token=${response.authResponse.accessToken}`;
+
+      // console.log(url);
+
+      // this.httpClient.get(url).subscribe((res) => {
+      //   console.log(res);
+      //   FB.logout((res) => {
+      //     console.log(res);
+      //     // window.location.reload();
+      //   });
+      // });
+    });
     // FB.login(function (response) {
     //   if (response.authResponse) {
     //     console.log("Welcome!  Fetching your information.... ");
