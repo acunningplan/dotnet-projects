a5447423c445d9b6b38e9de1d85bec44a7223b21
james-leung <jlfly12@gmail.com>
2020-10-20 17:40:24 +0100

    Added google map and marker

--- TravelBug/TravelBug.Web/ClientApp/src/app/google-map/google-map.component.ts
+++ TravelBug/TravelBug.Web/ClientApp/src/app/google-map/google-map.component.ts
@@ -1,44 +1,98 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { GmapService } from "../services/gmap.service";
 
 @Component({
-  selector: 'app-google-map',
-  templateUrl: './google-map.component.html',
-  styleUrls: ['./google-map.component.css']
+  selector: "app-google-map",
+  templateUrl: "./google-map.component.html",
+  styleUrls: ["./google-map.component.css"],
 })
 export class GoogleMapComponent implements OnInit, AfterViewInit {
+  componentInit = false;
+  componentInitSub: Subscription;
+  viewInit = false;
+  viewInitSub: Subscription;
+  gmap: google.maps.Map;
 
-  constructor() { }
+  @Input() height: string;
+  @Input() lat: number;
+  @Input() lng: number;
+  @ViewChild("mapContainer", { static: false }) elementRef: ElementRef;
 
+  constructor(private gmapService: GmapService) {}
+
+  // Make sure both the component and view are initiated
   ngOnInit() {
+    this.componentInit = true;
+    if (this.viewInit) this.mapInitialization();
   }
-
-
-
-  @ViewChild("mapContainer", { static: false }) gmap: ElementRef;
 
   ngAfterViewInit() {
-    this.mapInitializer();
+    this.viewInit = true;
+    if (this.componentInit) this.mapInitialization();
   }
 
-  mapInitializer() {
-    let lat = 40.73061;
-    let lng = -73.935242;
-    let coordinates = new google.maps.LatLng(lat, lng);
-
-    // Set map location
-    let mapOptions: google.maps.MapOptions = {
-      center: coordinates,
-      zoom: 13,
-    };
-
-    // Set marker location
-    let marker = new google.maps.Marker({
-      position: coordinates,
-    });
-
-    // Assign map to HTML element
-    let map = new google.maps.Map(this.gmap.nativeElement, mapOptions);
-    marker.setMap(map);
+  mapInitialization() {
+    if (this.lat && this.lng) {
+      let coords = { lat: this.lat, lng: this.lng };
+      this.gmapService.initMap(this.elementRef.nativeElement, coords);
+    } else {
+      this.gmapService.initMapWithUserLocation(this.elementRef.nativeElement);
+    }
   }
 
+  findUserLocation() {
+    // let map = new google.maps.Map(
+    //   document.getElementById("map") as HTMLElement,
+    //   {
+    //     center: { lat: 51.5073, lng: -0.12755 },
+    //     zoom: 8,
+    //   }
+    // );
+    let infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: Position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(this.elementRef.nativeElement);
+          this.elementRef.nativeElement.setCenter(pos);
+        },
+        () => {
+          this.handleLocationError(true, infoWindow, this.gmap.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      this.handleLocationError(false, infoWindow, this.gmap.getCenter());
+    }
+  }
+
+  handleLocationError(
+    browserHasGeolocation: boolean,
+    infoWindow: google.maps.InfoWindow,
+    pos: google.maps.LatLng
+  ) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(this.gmap);
+  }
 }
--- TravelBug/TravelBug.Web/ClientApp/src/app/new-blog/search-location/search-location.component.ts
+++ TravelBug/TravelBug.Web/ClientApp/src/app/new-blog/search-location/search-location.component.ts
@@ -1,112 +1,44 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { GmapService } from "src/app/services/gmap.service";
 
 @Component({
-  selector: 'app-search-location',
-  templateUrl: './search-location.component.html',
-  styleUrls: ['./search-location.component.css']
+  selector: "app-search-location",
+  templateUrl: "./search-location.component.html",
+  styleUrls: ["./search-location.component.css"],
 })
-export class SearchLocationComponent implements OnInit {
+export class SearchLocationComponent implements OnInit, OnDestroy {
   isSearching = false;
-  locationText = "Coffee";
-  proxyUrl = "https://cors-anywhere.herokuapp.com/"
+  isSearchingSub: Subscription;
+  coordsSub: Subscription;
+  locationText = "Sweeney and Todd";
+  proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
   coords: {
-    lat: number,
-    lng: number
-  }
+    lat: number;
+    lng: number;
+  };
 
-  constructor(private http: HttpClient) { }
+  constructor(private gmap: GmapService) {}
 
   ngOnInit() {
+    this.isSearchingSub = this.gmap.isSearching.subscribe(
+      (s) => (this.isSearching = s)
+    );
 
-    // let headers = new Headers();
-    // headers.append('Content-Type', 'application/json');
-    
-    // let options = new RequestOpt({ headers: headers });
-
-    // let proxyUrl = "https://cors-anywhere.herokuapp.com/"
-  }
-
-  types = ["locality", 'political']
-
-  searchHelper = () => {
-    this.isSearching = true;
-
-
-    // gapi.client.init({
-    //   'apiKey': environment.agmApiKey,
-    // }).then( () => gapi.client.request({
-    //     'path': requestUrl,
-    // })
-    // ).then(
-    //   res => {
-    //     console.log(res)
-    //     this.isSearching = false;
-    //   }
-    // , (reason)  => {
-    //   console.log('Error: ' + reason.result.error.message);
-    //   this.isSearching = false;
-    // });
-      let auth2 = gapi.auth2.init({
-        client_id: `${environment.googleClientId}.apps.googleusercontent.com`,
-        cookie_policy: "single_host_origin",
-        // scope: "profile email",
-      }).then (() => {
-
-
-
-    let requestUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${this.locationText}&inputtype=textquery&fields=formatted_address,name,geometry,types&key=${environment.agmApiKey}`        
-
-        gapi.load('client', () => {
-          gapi.client.init({
-          //   'apiKey': environment.agmApiKey,
-          }).then(() =>{
-            
-            gapi.client.request({
-                'path': 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
-                'params': {
-                  input: this.locationText,
-                  inputtype: 'textquery',
-                  fields: 'fomatted_address,name,geometry,types',
-                  key: environment.agmApiKey
-                }
-            })
-            .then(
-              res => {
-                console.log(res)
-                this.isSearching = false;
-              }
-            , (reason)  => {
-              console.log('Error: ' + reason.result.error.message);
-              this.
-              isSearching = false;
-            });
-
-          })
-        })
-
-      })
+    this.coordsSub = this.gmap.coordsSubject.subscribe((coords) => {
+      this.coords = coords;
+      console.log(this.coords);
+    });
   }
 
   searchLocation() {
-
-    gapi.load('auth2', this.searchHelper)  
-    // this.isSearching = true;
-    // let requestUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${this.locationText}&inputtype=textquery&fields=formatted_address,name,geometry,types&key=${environment.agmApiKey}`
-    // this.http
-    //   .get<PlacesApiResponse>(requestUrl)
-    //   .subscribe(res => {
-    //     this.isSearching = false;
-    //     if (res.candidates.length > 0) {
-    //       let place = res.candidates[0];
-    //       console.log(place)
-    //       this.coords = place.geometry.location;
-    //     } else {
-    //       console.log("Can't find anything.")
-    //     }
-    //   });
+    console.log("Searching");
+    this.gmap.searchLocation(this.locationText);
   }
 
+  ngOnDestroy() {
+    this.isSearchingSub.unsubscribe();
+    this.coordsSub.unsubscribe();
+  }
 }
