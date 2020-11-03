3da30a2f14c4d69e638acdccb198de9fc71d8c3f
James Leung <jlfly12@gmail.com>
2020-11-03 17:31:34 +0000

    Refactoring mock data for photo services

--- TravelBug/TravelBugTests/MockData.cs
+++ TravelBug/TravelBugTests/MockData.cs
@@ -6,13 +6,30 @@ using TravelBug.Entities.UserData;
 
 namespace TravelBugTests
 {
-  public class MockData
+  public static class MockData
   {
-    public List<AppUser> MockAppUsers { get; set; } = new List<AppUser>
+    public static List<AppUser> Users { get; } = new List<AppUser>
     {
         new AppUser {UserName = "ed"},
         new AppUser {UserName = "sarah"},
         new AppUser {UserName = "sam"},
     };
+    public static List<Blog> Blogs { get; } = new List<Blog>
+        {
+            new Blog {Title = "Ed's blog"},
+            new Blog {Title = "Sarah's first blog", Images = new List<Image> {
+
+            }},
+            new Blog {Title = "Sarah's second blog"},
+            new Blog {Title = "Sam's blog"},
+        };
+    public static List<Image> Images { get; } = new List<Image>
+        {
+            new Image {Url = "url1"},
+            new Image {Url = "url2"},
+            new Image {Url = "url3"},
+            new Image {Url = "url4"},
+        };
+
   }
 }
--- TravelBug/TravelBugTests/PhotoServicesTest.cs
+++ TravelBug/TravelBugTests/PhotoServicesTest.cs
@@ -7,75 +7,77 @@ using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System;
 using TravelBug.Infrastructure.PhotoLogic;
+using System.Collections.Generic;
+using TravelBug.Entities.UserData;
 
 namespace TravelBugTests
 {
-  public class PhotoServicesTest
-  {
-    private PhotoService _photoService;
-    private Mock<IPhotoService> _photoServiceMock = new Mock<IPhotoService>();
-    private readonly MockData mockData = new MockData();
-    private readonly DbContextOptions<TravelBugContext> _options;
-    private readonly Mock<IUserAccessor> _userAccessorMock = new Mock<IUserAccessor>();
-
-    // Save mock blogs and users (not from database)
-
-
-    public PhotoServicesTest()
+    public class PhotoServicesTest
     {
-      var optionsBuilder = new DbContextOptionsBuilder<TravelBugContext>();
-      optionsBuilder.UseSqlite("Data Source=blog.db");
-      _options = optionsBuilder.Options;
+        private PhotoService _photoService;
+        private List<AppUser> _users;
+        private Mock<IPhotoService> _photoServiceMock = new Mock<IPhotoService>();
+        private readonly DbContextOptions<TravelBugContext> _options;
+        private readonly Mock<IUserAccessor> _userAccessorMock = new Mock<IUserAccessor>();
 
-      // Set up users
-      MockUserAccessor.SetupMock(_userAccessorMock);
-    }
+        // Save mock blogs and users (not from database)
 
-    [Fact]
-    public async Task ShouldGetPhotoByUrl()
-    {
-      using (var context = new TravelBugContext(_options))
-      {
-        // Arrange
-        Seed.SeedData(context);
-        _photoService = new PhotoService(context, _userAccessorMock.Object);
 
-        // Action
-        var photo1 = await _photoService.GetPhotoByUrl("url1");
-        var photo2 = await _photoService.GetPhotoByUrl("url0");
-
-        // Assert
-        Assert.NotNull(photo1);
-        Assert.Null(photo2);
-      }
-    }
-
-    [Fact]
-    public async Task ShouldSaveProfilePicture()
-    {
-      using (var context = new TravelBugContext(_options))
-      {
-        // Arrange
-        Seed.SeedData(context);
-        _photoService = new PhotoService(context, _userAccessorMock.Object);
-
-        var responseObject = new PhotoUploadResponse
+        public PhotoServicesTest()
         {
-          Data = new ResponseData
-          {
-            Id = "imgurId1",
-            Link = "link1",
-            DeleteHash = "deleteHash1"
-          }
-        };
+            var optionsBuilder = new DbContextOptionsBuilder<TravelBugContext>();
+            optionsBuilder.UseSqlite("Data Source=blog.db");
+            _options = optionsBuilder.Options;
 
-        // Action
-        await _photoService.SaveProfilePicture(responseObject);
-        var user = await context.Users.FirstAsync(u => u.UserName == "ed");
+            // Set up users
+            _users = MockData.Users;
+            MockUserAccessor.SetupMock(_userAccessorMock, _users);
+        }
 
-        // Assert
-        // Assert.Equal("imgurId1", user.ProfilePicture.ImgurId);
-      }
+        [Fact]
+        public async Task ShouldGetPhotoByUrl()
+        {
+            using (var context = new TravelBugContext(_options))
+            {
+                // Arrange
+                Seed.SeedData(context);
+                _photoService = new PhotoService(context, _userAccessorMock.Object);
+
+                // Action
+                var photo1 = await _photoService.GetPhotoByUrl("url1");
+                var photo2 = await _photoService.GetPhotoByUrl("url0");
+
+                // Assert
+                Assert.NotNull(photo1);
+                Assert.Null(photo2);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldSaveProfilePicture()
+        {
+            using (var context = new TravelBugContext(_options))
+            {
+                // Arrange (mock profile picture)
+                Seed.SeedData(context);
+                _photoService = new PhotoService(context, _userAccessorMock.Object);
+
+                var responseObject = new PhotoUploadResponse
+                {
+                    Data = new ResponseData
+                    {
+                        Id = "imgurId1",
+                        Link = "link1",
+                        DeleteHash = "deleteHash1"
+                    }
+                };
+
+                // Action (set profile photo for Ed)
+                await _photoService.SaveProfilePicture(responseObject);
+
+                // Assert (Ed's profile picture)
+                Assert.Equal("imgurId1", _users[0].ProfilePicture.ImgurId);
+            }
+        }
     }
-  }
 }
