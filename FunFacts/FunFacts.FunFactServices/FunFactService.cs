3fba3ee2ec704a668e04318818874e823c5d8e19
James Leung <jlfly12@gmail.com>
2020-11-21 11:12:02 +0000

    Untrack unneeded compile files

--- FunFacts/FunFacts.FunFactServices/FunFactService.cs
+++ FunFacts/FunFacts.FunFactServices/FunFactService.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿using FunFacts.Context;
+using FunFacts.Entities;
+using System.Threading.Tasks;
 
-namespace FunFacts.FunFactService
+namespace FunFacts.FunFactServices
 {
-    public class FunFactService
+    public class FunFactService : IFunFactService
     {
+        private readonly FunFactsContext _context;
+
+        public FunFactService(FunFactsContext context)
+        {
+            _context = context;
+        }
+        public async Task AddFunFact(FunFact funFact)
+        {
+            _context.FunFacts.Add(new FunFact());
+            await _context.SaveChangesAsync();
+        }
     }
 }
